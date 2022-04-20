6e1f2829dec74125b8d197ccb6941503ac5d3022
SecondPort <gomezmanulm@gmail.com>
2022-04-20 20:27:51 -0300

    claseRegistro

--- Java/src/Java/edu.ubp.doo.modelo/Registro.java
+++ Java/src/Java/edu.ubp.doo.modelo/Registro.java
@@ -1,10 +1,12 @@
 package edu.ubp.doo.modelo;
 
+/*
 public class Registro{
     private List<Vehiculo> listadoVehiculos;
     Registro(){
         listadoVehiculos = new ArrayList<Vehiculo>();
     }
+
     public void agregarVehiculo(Vehiculo vehiculo){
         listadoVehiculos.add(vehiculo);
     }
@@ -13,6 +15,24 @@ public class Registro{
     }
     public void mostrarVehiculos(){
         for(Vehiculo vehiculo : listadoVehiculos){
+            System.out.println(vehiculo);
+        }
+    }
+}*/
+
+public class Registro {
+    List<Vehiculo> listado;
+
+    public void cargarListado(List<Vehiculo> lista){
+        this.listado = lista;
+    }
+
+    public void cargarListado(Vehiculo...lista){
+        this.listado = Arrays.asList(lista);
+    }
+
+    public void mostrarListado(){
+        for(Vehiculo vehiculo : listado){
             System.out.println(vehiculo);
         }
     }
